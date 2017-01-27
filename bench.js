70c92586ee7fded140c7cc70fdb8ee0c6fb98179
Dustin Dowell <dustindowell22@gmail.com>
2017-01-27 02:56:02 -0600

    Bench pixie

--- bench.js
+++ bench.js
@@ -1,20 +1,30 @@
 var Suite = require('benchmark').Suite
-var tempest = require('./')
-var compile = tempest.compile
-var render = tempest.render
-
 var test = new Suite()
 
-test.add('parse', function() {
-  tempest('foo{{bar}}baz{{qux}}qix')
-})
+var s = 'foo{{bar}}baz{{qux}}qix'
+var d = { bar: ' BAR ', qux: ' QUX ' }
 
-const sample = tempest('foo{{bar}}baz{{qux}}qix')
-test.add('compile', function() {
-  compile(sample, { bar: ' BAR ', qux: ' QUX ' })
-})
+var tempest = require('./')
+var tempestCompile = tempest.compile
+var tempestRender = tempest.render
 
-test.on('cycle', function(event) {
+var t = tempest('foo{{bar}}baz{{qux}}qix')
+
+test.add('tempest        ', () => { tempest(s) })
+test.add('tempest compile', () => { tempestCompile(t, d) })
+test.add('tempest render ', () => { tempestRender(s, d) })
+
+var pixie = require('pixie')
+var pixieCompile = pixie.compile
+var pixieRender = pixie.render
+
+var t = pixie('foo{{bar}}baz{{qux}}qix')
+
+test.add('pixie          ', () => { pixie(s) })
+test.add('pixie compile  ', () => { pixieCompile(t, d) })
+test.add('pixie render   ', () => { pixieRender(s, d) })
+
+test.on('cycle', event => {
   console.log(String(event.target))
 })
 
