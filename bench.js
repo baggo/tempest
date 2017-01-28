0f96b2243eb5b9630d978e8bb6aa1f6f2de06004
Dustin Dowell <dustindowell22@gmail.com>
2017-01-28 14:17:42 -0600

    Update bench

--- bench.js
+++ bench.js
@@ -1,28 +1,30 @@
 var Suite = require('benchmark').Suite
 var test = new Suite()
 
-var s = 'foo{{bar}}baz{{qux}}qix'
-var d = { bar: ' BAR ', qux: ' QUX ' }
-
 var tempest = require('./')
 var tempestCompile = tempest.compile
 var tempestRender = tempest.render
-
-var t = tempest('foo{{bar}}baz{{qux}}qix')
-
-test.add('tempest        ', () => { tempest(s) })
-test.add('tempest compile', () => { tempestCompile(t, d) })
-test.add('tempest render ', () => { tempestRender(s, d) })
 
 var pixie = require('pixie')
 var pixieCompile = pixie.compile
 var pixieRender = pixie.render
 
-var t = pixie('foo{{bar}}baz{{qux}}qix')
+var balanced = require('balanced-match')
 
-test.add('pixie          ', () => { pixie(s) })
-test.add('pixie compile  ', () => { pixieCompile(t, d) })
-test.add('pixie render   ', () => { pixieRender(s, d) })
+var s = 'foo{{bar}}baz{{qux}}qix' + 'foo{{bar}}baz{{qux}}qix' + 'foo{{bar}}baz{{qux}}qix' + 'foo{{bar}}baz{{qux}}qix'
+var d = { bar: ' BAR ', qux: ' QUX ' }
+
+var t = tempest(s)
+test.add('tempest         ', () => { tempest(s) }, { minSamples: 100 })
+test.add('tempest compile ', () => { tempestCompile(t, d) })
+test.add('tempest render  ', () => { tempestRender(s, d) })
+
+var t = pixie(s)
+test.add('pixie           ', () => { pixie(s) }, { minSamples: 100 })
+test.add('pixie compile   ', () => { pixieCompile(t, d) })
+test.add('pixie render    ', () => { pixieRender(s, d) })
+
+test.add('balanced        ', () => { balanced('{{', '}}', s) })
 
 test.on('cycle', event => {
   console.log(String(event.target))
